eda375e1904b1fbaebec46a5ffe57d0195f65c74
Erdal Mersinlioglu <erdal.mersinlioglu@bahag.com>
2022-01-08 02:19:46 +0100

    Excel form style

---
+++ models/Customer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "customer".
+ *
+ * @property int $id
+ * @property string|null $company_name
+ */
+class Customer extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'customer';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['company_name'], 'string', 'max' => 512],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'company_name' => 'Company Name',
+        ];
+    }
+}
